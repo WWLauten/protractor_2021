aa2db106065915f9679bf4cdcd053e498864b567
Willian W. Lautenschlager <willianw@uol.com.br>
2021-07-24 22:42:46 -0300

    Section 4 Lesson 5

--- e2e/src/photo-detail.po.ts
+++ e2e/src/photo-detail.po.ts
@@ -1,4 +1,4 @@
-import { browser } from "protractor";
+import { browser, by, element } from "protractor";
 
 export class PhotoDetailPage {
 
@@ -8,4 +8,19 @@ export class PhotoDetailPage {
         return browser.getTitle();
     }
 
+    fillComment(text: string) {
+        return element(by.css('textarea.form-control'))
+            .sendKeys(text);
+    }
+
+    publishComment() {
+        return element(by.css('button[type=submit]'))
+            .click();
+    }
+
+    getCommentListSize() {
+        return element
+            .all(by.css('ap-photo-comments li'))
+            .count();
+    }
 }
